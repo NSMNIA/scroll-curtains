b7bb08251dce2f8b827fb0483befe45f51676cfd
NSMNIA <kevin.stoop@revolveddesign.com>
2022-09-28 21:44:41 +0200

    updated readme
    Signed-off-by: NSMNIA <kevin.stoop@revolveddesign.com>

--- hooks/useScrollview.tsx
+++ hooks/useScrollview.tsx
@@ -7,13 +7,11 @@ const useScrollViewEffect = () => {
     const progress = useMotionValue(0);
 
     useEffect(() => {
-        const elemRect = ref.current?.getBoundingClientRect();
+        const rekt = ref.current?.getBoundingClientRect();
         const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-        const updateProgress = (scrollYValue: number) => progress.set(Math.min(Math.max((scrollYValue - elemRect.top + windowHeight) / windowHeight, 0), 2));
+        const updateProgress = (scrollYValue: number) => progress.set(Math.min(Math.max((scrollYValue - rekt.top + windowHeight) / windowHeight, 0), 2));
         const unsubscribeScrollY = scrollY.onChange(updateProgress);
-        return () => {
-            unsubscribeScrollY();
-        };
+        return () => unsubscribeScrollY();
     }, [progress, scrollY]);
 
     return [ref, progress];
